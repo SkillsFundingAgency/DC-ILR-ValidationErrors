9a63a3a85a0f8e82bfd26e473c65b0b2c3857bec
Salman Baig <salman.sfa@outlook.com>
2018-05-29 16:35:43 +0100

    added interface and model

---
+++ src/ESFA.DC.ValidationErrors/ValidationErrorsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.ValidationErrors
+{
+    public class ValidationErrorsService
+    { 
+        
+    }
+}
